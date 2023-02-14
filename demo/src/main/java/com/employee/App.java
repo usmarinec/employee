acd6a58728b13f5f5d21b095af582f2c52ba93c7
Lon <usmarinec@gmail.com>
2023-02-13 17:22:43 -0700

    App

--- demo/src/main/java/com/employee/App.java
+++ demo/src/main/java/com/employee/App.java
@@ -1,13 +1,16 @@
 package com.employee;
 
-/**
- * Hello world!
- *
- */
+import com.employee.classes.Developer;
+import com.employee.classes.HumanResources;
+import com.employee.classes.Developer.Language;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Developer employee = new Developer("Glass Joe", 1234, Language.JAVA);
+        employee.register(new HumanResources());
+        employee.setOrgNum(9876);
+        employee.notifyObservers();
     }
 }
